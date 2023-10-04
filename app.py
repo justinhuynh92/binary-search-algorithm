87ca4e8bf76a6e7862b72d9e34e2df5dae162266
Justin Huynh <justinhuynh639@gmail.com>
2023-10-04 09:44:59 -0700

    showing start and end of list

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ def binary_search(list, element):
     end = len(list)
     steps = 0
 
+    while(start<=end):
+        #show the start and end of the list
+        print('Step', steps, ":" , str(list[start:end+1]))
+
 # multiple variables : middle, start, end, steps
 # recursion or while loop
 # increase the steps each time a split is done
