14cf3594a3fe2ca5302da475072bc5daa16a6559
Justin Huynh <justinhuynh639@gmail.com>
2023-10-04 09:49:13 -0700

    track positions

--- app.py
+++ app.py
@@ -5,11 +5,22 @@ def binary_search(list, element):
     end = len(list)
     steps = 0
 
+    # recursion or while loop
     while(start<=end):
         #show the start and end of the list
         print('Step', steps, ":" , str(list[start:end+1]))
 
+        # increase the steps each time a split is done
+        steps = steps + 1
+        middle = (start + end) // 2
+
+        # conditions to track target position
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+
+
 # multiple variables : middle, start, end, steps
-# recursion or while loop
-# increase the steps each time a split is done
-# conditions to track target position
