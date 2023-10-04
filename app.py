c771d29e004f3154bb9c152a6b802ecafa6ffb04
Justin Huynh <justinhuynh639@gmail.com>
2023-10-04 09:41:52 -0700

    started binary search algo

---
+++ app.py
@@ -0,0 +1,11 @@
+# a function that takes a list and target parameter
+def binary_search(list, element):
+    middle = 0
+    start = 0
+    end = len(list)
+    steps = 0
+
+# multiple variables : middle, start, end, steps
+# recursion or while loop
+# increase the steps each time a split is done
+# conditions to track target position
